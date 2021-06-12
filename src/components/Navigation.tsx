d306ee76dbe62c9ee15ef87407f8587cf25881f6
Jan Eliasz <johnbg@tlen.pl>
2021-06-12 09:04:57 +0200

    Global CSS variables

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -12,12 +12,11 @@ interface NavigationProps {
 function Navigation({ open, data, toggler }: NavigationProps){
     return (
         <nav className={`${styles.navigation} ${open ? styles.open : ''}`}>
-            <ul>
+            <ul className={styles.navList}>
                 {data.map((item, idx) => {
                     return (
                         <Link key={idx} to={`/${item.name}`}>
-                            <li className={styles.navItem} onClick={() => toggler(prev => !prev)}>
-                                <div className={styles.bullet} style={{background: `${item.color}`}}></div>
+                            <li className={`${styles.navItem} ${styles[`${item.name.toLowerCase()}`]}`} onClick={() => toggler(prev => !prev)}>
                                 <span className={styles.navItemText}>{item.name}</span>
                             </li>
                         </Link>
