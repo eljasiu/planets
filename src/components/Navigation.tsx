b7fe5fe09ce12f222ef66e0ffeda7b5368d960af
Jan Eliasz <johnbg@tlen.pl>
2021-06-12 09:20:29 +0200

    Header for tablets

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,7 +15,7 @@ function Navigation({ open, data, toggler }: NavigationProps){
             <ul className={styles.navList}>
                 {data.map((item, idx) => {
                     return (
-                        <Link key={idx} to={`/${item.name}`}>
+                        <Link key={idx} to={`/${item.name.toLowerCase()}`}>
                             <li className={`${styles.navItem} ${styles[`${item.name.toLowerCase()}`]}`} onClick={() => toggler(prev => !prev)}>
                                 <span className={styles.navItemText}>{item.name}</span>
                             </li>
