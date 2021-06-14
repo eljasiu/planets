4acf2a2d83d078fe12a65307597fd0b60a386741
Jan Eliasz <johnbg@tlen.pl>
2021-06-14 08:50:36 +0200

    Planet graphics mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import './App.css';
 import Header from './components/Header';
 import data from './data';
 import { Route, Redirect } from 'react-router-dom';
-///import usePlanet from './hooks/usePlanet';
-//import Planet from './interfaces/planet';
 import { useState } from 'react';
 import Tabs from './components/Tabs';
+import Graphics from './components/Graphics';
 
 function App() {
   const [tab, setTab] = useState(0);
-  //const planet: Planet = usePlanet();
 
   return (
     <div className="App">
@@ -20,6 +18,7 @@ function App() {
         <Header data={data}/>
         <div className="grid">
           <Tabs tab={tab} setTab={setTab}/>
+          <Graphics tab={tab} />
         </div>
     </div>
   );
